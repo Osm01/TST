b9b8ec470de4e55721bf8069a704080c23b1a2e2
Osm01 <44633928+Osm01@users.noreply.github.com>
2022-06-06 17:39:12 +0100

    use delegate and action

---
+++ #1_Test_Staff/Assets/Script/TST code/TSTCode.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+using UnityEngine.UI;
+public class TSTCode : MonoBehaviour
+{
+    public TextMeshProUGUI textMeshProUGUI;
+    public Button btnYes;
+    public Button btnNo;
+    private void Awake()
+    {
+        string qt = "Do u want to quit the Game!!";
+        ShowDialog(qt, () => { Debug.Log("yes"); },() => { Debug.Log("No"); }) ;
+    }
+    void ShowDialog(string TxtDialog,Action actionYes,Action actionNo)
+    {
+        textMeshProUGUI.text = TxtDialog;
+        //if button preess trigger the action (two way of trigger button with action)
+        btnYes.onClick.AddListener(() => { actionYes(); });
+        btnNo.onClick.AddListener(new UnityEngine.Events.UnityAction(actionNo));
+    }
+}
